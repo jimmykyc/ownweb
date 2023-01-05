629c7e5b81e6d7d4dd3c014934734be6ef402dc8
Jimmy Dev <81708489+jimmykyc@users.noreply.github.com>
2023-01-05 16:25:12 +0800

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,87 @@
+var telegram = "https://t.me/xit_thematrix";
+var twitter = "https://twitter.com/exit_thematrix1";
+var email = 'mailto:enter@the-matrix.xyz';
+
+whoare = [
+  "<br>",
+  "Hey, we are you a good client?👋",
+  "Come and let me know what you need!<br>",
+  "I can guarantee you a good quality of my works",
+  "Free to edit anytime you have something to change on you website orders.",
+  "Message me anytime you need me."
+];
+
+whoami = [
+  "<br>",
+  "I am just a normal website and graphics developer on crypto and legit services i can give you",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'telegram        <a href="' + telegram + '" target="_blank">https://t.me/webservice2022' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">https://twitter.com/devservice2022' + "</a>",
+  "<br>"
+];
+
+
+service = [
+  "<br>",
+  "WEBSITE",
+  "■ Graphic based on logo",
+  "■ Free Domian",
+  "<br>",
+  "BUYBOT",
+  "■ Video",
+  "■ Gif",
+  "<br>",
+  "GROUP BOT",
+  "■ Welcome Bot",
+  "■ Buy Bot"
+];
+
+faq = [
+  "<br>",
+  "■ ARE YOU SAFE?",
+  "Well. No one is safe in this world. You mean if your funds are safe with us. Yes they are.",
+  "<br>",
+  "■ WHEN WILL THE ORDERS DONE?",
+  "Your order will be out with in 4 - 6 hrs.",
+  "<br>",
+  "■ IS THE DOMAIN FREE?",
+  "Yes The domain is free to all website orders.",
+  "<br>",
+  "■ DO YOU HAVE ANOTHER SKILLS BESIDE WEBSITE?",
+  "Yes BUYBOT VIDEO/GIF, LOGO, and WELCOME BOT/BUYBOT with your project logo.",
+  "<br>",
+  "■ HOW MUCH IT COST?",
+  "WEBSITE - 0.15 ETH",
+  "BUYBOT VIDEO/GIF - 0.1 ETH",
+  "LOGO - 0.05 ETH",
+  "BUYBOT/WELCOME BOT - 0.05ETH",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whoare</span>         Who Are We?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">Service</span>        View Services',
+  '<span class="command">faq</span>            View the FAQ',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<div style="display: inline-flex; align-items: left;">',
+  '<span class="index">@2023 | Jimmy Service Agency. All rights reserved. <br>Coded with ❤️ by <a href="https://t.me/webservice2022" target="_blank">jimmyservice.xyz</a></span>',
+  '<img style="margin-right: 2em; with: 100px !important;" src="logo2.png"/>',
+    '</div>',
+  '<span class="color2">Greetings programs. Its time to EXIT the PANEL.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
